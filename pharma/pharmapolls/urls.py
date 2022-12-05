3196569eac23c981e04203e0d4c08e13b3a5eb16
ramziddin1671 <81315015+ramziddin1671@users.noreply.github.com>
2022-12-05 14:00:54 +0500

    urls va viewlar qo'shildi
    urls va viewlar qo'shildi

--- pharma/pharmapolls/urls.py
+++ pharma/pharmapolls/urls.py
@@ -1,8 +1,28 @@
 from django.urls import path
-from .views import OrganizationList, OrganizationDetail
+from .views import *
 
 
 urlpatterns = [
     path("<int:pk>/", OrganizationDetail.as_view()),
     path("", OrganizationList.as_view()),
+    path("Author/", AuthorList.as_view()),
+    path("Author/<int:pk>/", AuthorDetail.as_view()),
+    path("Jurnal/", JurnalList.as_view()),
+    path("Jurnal/<int:pk>/", JurnalDetail.as_view()),
+    path("Subdivision/", SubdivisionList.as_view()),
+    path("Subdivision/<int:pk>/", SubdivisionDetail.as_view()),
+    path("Statya/", StatyaList.as_view()),
+    path("Statya/<int:pk>/", StatyaDetail.as_view()),
+    path("Conference/", ConferenceList.as_view()),
+    path("Conference/<int:pk>/", ConferenceDetail.as_view()),
+    path("Seminar/", SeminarList.as_view()),
+    path("Seminar/<int:pk>/", SeminarDetail.as_view()),
+    path("Video/", VideoList.as_view()),
+    path("Video/<int:pk>/", VideoDetail.as_view()),
+    path("Video_Gallery/", Video_GalleryList.as_view()),
+    path("Video_Gallery/<int:pk>/", Video_GalleryDetail.as_view()),
+    path("News/", NewsList.as_view()),
+    path("News/<int:pk>/", NewsDetail.as_view()),
+    path("Contact/", ContactList.as_view()),
+    path("Contact/<int:pk>/", ContactDetail.as_view()),
 ]
--- pharma/pharmapolls/views.py
+++ pharma/pharmapolls/views.py
@@ -15,3 +15,113 @@ class OrganizationDetail(RetrieveUpdateDestroyAPIView):
     permission_classes = (IsAuthenticatedOrReadOnly, )
     queryset = Organization.objects.all()
     serializer_class = OrganizationSerializer
+
+
+class AuthorList(ListCreateAPIView):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+
+
+class AuthorDetail(RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthenticatedOrReadOnly, )
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+
+
+class JurnalList(ListCreateAPIView):
+    queryset = Jurnal.objects.all()
+    serializer_class = JurnalSerializer
+
+
+class JurnalDetail(RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+    queryset = Jurnal.objects.all()
+    serializer_class = JurnalSerializer
+
+
+class SubdivisionList(ListCreateAPIView):
+    queryset = Subdivision.objects.all()
+    serializer_class = SubdivisionSerializer
+
+
+class SubdivisionDetail(RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+    queryset = Subdivision.objects.all()
+    serializer_class = SubdivisionSerializer
+
+
+class StatyaList(ListCreateAPIView):
+    queryset = Statya.objects.all()
+    serializer_class = StatyaSerializer
+
+
+class StatyaDetail(RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+    queryset = Statya.objects.all()
+    serializer_class = StatyaSerializer
+
+
+class ConferenceList(ListCreateAPIView):
+    queryset = Conference.objects.all()
+    serializer_class = ConferenceSerializer
+
+
+class ConferenceDetail(RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+    queryset = Conference.objects.all()
+    serializer_class = ConferenceSerializer
+
+
+class SeminarList(ListCreateAPIView):
+    queryset = Seminar.objects.all()
+    serializer_class = SeminarSerializer
+
+
+class SeminarDetail(RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+    queryset = Seminar.objects.all()
+    serializer_class = SeminarSerializer
+
+
+class VideoList(ListCreateAPIView):
+    queryset = Video.objects.all()
+    serializer_class = VideoSerializer
+
+
+class VideoDetail(RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+    queryset = Video.objects.all()
+    serializer_class = VideoSerializer
+
+
+class Video_GalleryList(ListCreateAPIView):
+    queryset = Video_Gallery.objects.all()
+    serializer_class = Video_GallerySerializer
+
+
+class Video_GalleryDetail(RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+    queryset = Video_Gallery.objects.all()
+    serializer_class = Video_GallerySerializer
+
+
+class NewsList(ListCreateAPIView):
+    queryset = News.objects.all()
+    serializer_class = NewsSerializer
+
+
+class NewsDetail(RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+    queryset = News.objects.all()
+    serializer_class = NewsSerializer
+
+
+class ContactList(ListCreateAPIView):
+    queryset = Contact.objects.all()
+    serializer_class = ContactSerializer
+
+
+class ContactDetail(RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+    queryset = Contact.objects.all()
+    serializer_class = ContactSerializer
