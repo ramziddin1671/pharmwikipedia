59ae5b730a75ad417c95d039fb57c5cf031d9756
Dilmurod7007 <habibullayev.1997@mail.ru>
2022-12-13 20:56:24 +0500

    nothing

--- pharma/pharma/settings.py
+++ pharma/pharma/settings.py
@@ -36,8 +36,13 @@ INSTALLED_APPS = [
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES':[
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
-    ]
-}
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication'
+    ],
+    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema'
+    }
+
 
 
 
